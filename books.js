c438c32231e17371e55a78aef23972240b6cc1eb
lizconlan <lizconlan@gmail.com>
2014-06-05 23:17:13 +0100

    Toggle title/publisher sort

---
+++ books.js
@@ -0,0 +1,29 @@
+function sort_by_publisher() {
+  $('article').sortElements(function(a, b){
+    pub_a = $(a).find('p[itemprop="publisher"]')[0].textContent.trim();
+    title_a = $(a).find('h2')[0].textContent.trim();
+    sort_key_a = pub_a.trim() + "__" + title_a.trim().replace(/^(The )|(A )/, "");
+    
+    pub_b = $(b).find('p[itemprop="publisher"]')[0].textContent.trim();
+    title_b = $(b).find('h2')[0].textContent.trim();
+    sort_key_b = pub_b.trim() + "__" + title_b.trim().replace(/^(The )|(A )/, "");
+    
+    return sort_key_a.toLowerCase().localeCompare(sort_key_b.toLowerCase());
+  });
+  $('#pub_sort').hide();
+  $('#title_sort').show();
+}
+
+function sort_by_title() {
+  $('article').sortElements(function(a, b){
+    title_a = $(a).find('h2')[0].textContent.trim();
+    sort_key_a = title_a.trim().replace(/^(The )|(A )/, "");
+    
+    title_b = $(b).find('h2')[0].textContent.trim();
+    sort_key_b = title_b.trim().replace(/^(The )|(A )/, "");
+    
+    return sort_key_a.toLowerCase().localeCompare(sort_key_b.toLowerCase());
+  });
+  $('#title_sort').hide();
+  $('#pub_sort').show();
+}
