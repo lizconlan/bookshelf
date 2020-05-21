21df054f516381145baaac3003f17e8e4d08b504
Liz Conlan <lizconlan@gmail.com>
2020-05-21 16:12:36 +0100

    Add Edition class

--- bin/bookshelf.rb
+++ bin/bookshelf.rb
@@ -286,3 +286,25 @@ class Book
     files
   end
 end
+
+class Edition
+  attr_reader :ident, :isbn, :authors, :editors, :notes, :formats, :book,
+              :title, :link, :publisher
+
+  def initialize(book, ident: nil, isbn: nil, authors: nil, notes: nil, formats: nil, title: nil, folder_name: nil)
+    @book = book
+    @folder_name = folder_name
+    @isbn = isbn || book.isbn
+    @ident = ident || @isbn
+    @authors = authors || book.authors
+    @notes = notes || book.notes
+    @formats = formats || book.formats
+    @title =
+      if title
+        "#{book.title} - #{title}"
+      else
+        book.title
+      end
+    @publisher = book.publisher
+  end
+end
---
+++ bin/test/test_edition.rb
@@ -0,0 +1,58 @@
+require_relative "test_helper"
+require_relative "../bookshelf"
+
+class TestEdition < Minitest::Test
+
+  describe '.new' do
+    let(:book) { Book.new }
+    let(:edition) { Edition.new(book) }
+
+    describe 'sensible defaults' do
+      it { assert_equal(book, edition.book) }
+      it { assert_equal([], edition.authors) }
+      it { assert_empty(edition.publisher) }
+      it { assert_empty(edition.isbn) }
+      it { assert_empty(edition.ident) }
+      it { assert_empty(edition.notes) }
+      it { assert_nil(edition.title) }
+    end
+
+    describe 'no book passed in' do
+      it { assert_raises(ArgumentError) { Edition.new } }
+    end
+
+    describe 'values inherited from book' do
+      let(:book) do
+        Book.new(
+          isbn: '8601404204708',
+          authors: ["William Shakespeare"],
+          notes: "2007 version",
+          title: "The Complete Works"
+        )
+      end
+
+      it { assert_equal(book.isbn, edition.isbn) }
+      it { assert_equal(book.authors, edition.authors) }
+      it { assert_equal(book.notes, edition.notes) }
+      it { assert_equal(book.title, edition.title) }
+    end
+
+    describe 'inherited values can be overridden by data passed in' do
+      let(:book) do
+        Book.new(
+          isbn: '8601404204708',
+          authors: ["William Shakespeare"],
+          notes: "2007 version",
+          title: "The Complete Works"
+        )
+      end
+
+      let(:edition) { Edition.new(book, notes: "2nd printing", title: "2008 edit") }
+
+      it { assert_equal(book.isbn, edition.isbn) }
+      it { assert_equal("2nd printing", edition.notes) }
+      it { assert_equal("The Complete Works - 2008 edit", edition.title) }
+    end
+  end
+
+end
