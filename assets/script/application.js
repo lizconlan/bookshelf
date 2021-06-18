523ba741e37dbe7cc1609a83ee5b3bb99cbd6aa4
Liz Conlan <lizconlan@gmail.com>
2021-06-18 15:17:52 +0100

    Strip out jQuery

--- assets/script/application.js
+++ assets/script/application.js
@@ -1,23 +1,30 @@
 (() => {
   const application = Stimulus.Application.start()
 
+  let sortElements = function(items, sorter) {
+    var targetElement = items[0].parentElement
+
+    Array.from(targetElement.children).forEach(node=>targetElement.removeChild(node))
+    items.sort(sorter).forEach(node=>targetElement.appendChild(node))
+  }
+
   let titleSort = function(a, b) {
-    let title_a = $(a).find("h2")[0].textContent.trim()
+    let title_a = Array.from(a.querySelectorAll("h2"))[0].textContent.trim()
     let sort_key_a = title_a.trim().replace(/^(The )|(A )/, "")
 
-    let title_b = $(b).find("h2")[0].textContent.trim()
+    let title_b = Array.from(b.querySelectorAll("h2"))[0].textContent.trim()
     let sort_key_b = title_b.trim().replace(/^(The )|(A )/, "")
 
     return sort_key_a.toLowerCase().localeCompare(sort_key_b.toLowerCase())
   }
 
   let publisherSort = function(a, b){
-    let pub_a = $(a).find("p[itemprop='publisher']")[0].textContent.trim()
-    let title_a = $(a).find("h2")[0].textContent.trim()
+    let pub_a = Array.from(a.querySelectorAll("p[itemprop='publisher']"))[0].textContent.trim()
+    let title_a = Array.from(a.querySelectorAll("h2"))[0].textContent.trim()
     let sort_key_a = pub_a.trim() + "__" + title_a.trim().replace(/^(The )|(A )/, "")
 
-    let pub_b = $(b).find("p[itemprop='publisher']")[0].textContent.trim()
-    let title_b = $(b).find("h2")[0].textContent.trim()
+    let pub_b = Array.from(b.querySelectorAll("p[itemprop='publisher']"))[0].textContent.trim()
+    let title_b = Array.from(b.querySelectorAll("h2"))[0].textContent.trim()
     let sort_key_b = pub_b.trim() + "__" + title_b.trim().replace(/^(The )|(A )/, "")
 
     return sort_key_a.toLowerCase().localeCompare(sort_key_b.toLowerCase())
@@ -25,26 +32,17 @@
 
   application.register("start", class extends Stimulus.Controller {
     initialize() {
-      $("h1.fallback").css("color", "#fff")
-      $("header").height(110)
-      $("#nav_info").show()
-      $("#filters").show()
-      $(".pubfilter").val("Show All")
-      $("#pub_sort").show()
-      $("#search").show()
-      $("#search_box").val("")
-      $("#fade").hide()
-      $("img[itemprop='image']").hover(function() {
-        $(this).css("cursor","pointer")
-      })
-
-      // case insensitive modifier for jQuery :contains
-      // lifted from CSS Tricks
-      // https://css-tricks.com/snippets/jquery/make-jquery-contains-case-insensitive/
-      $.expr[":"].contains = $.expr.createPseudo(function(arg) {
-        return function(elem) {
-          return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0
-        }
+      document.querySelector('h1.fallback').style.color = "#fff"
+      document.querySelector("header").style.height = "110"
+      document.querySelector("#nav_info").style.display = "block"
+      document.querySelector("#filters").style.display = "block"
+      document.querySelector(".pubfilter").selectedElement = 0
+      document.querySelector("#pub_sort").style.display = "block"
+      document.querySelector("#search").style.display = "block"
+      document.querySelector("#search_box").value = ""
+      document.querySelector("#fade").style.display = ""
+      document.querySelectorAll("img[itemprop='image']").forEach((element, idx) => {
+        element.style.cursor = "pointer"
       })
     }
   })
@@ -55,13 +53,13 @@
     }
 
     reveal_info() {
-      $("#fade").show()
-      $(this.panelTarget).show()
-      if(this.tabsetTarget) {
+      document.querySelector('#fade').style.display = "block"
+      this.panelTarget.style.display = "block"
+      if(this.hasTabsetTarget) {
         this.contentTargets.forEach((element, idx) => {
-          $(element).hide()
+          element.style.display = ""
         })
-        $(this.contentTarget).show()
+        this.contentTarget.style.display = "block"
         this.buttonTarget.classList.add("selected")
       }
     }
@@ -70,91 +68,111 @@
       let idx = evt.target.dataset.idx
 
       // unselect all buttons
-      this.buttonTargets.forEach((element, idx) => {
+      this.buttonTargets.forEach((element) => {
         element.classList.remove("selected")
       })
       this.buttonTargets[idx].classList.add("selected")
 
       // hide open panels
-      this.contentTargets.forEach((element, idx) => {
-        $(element).hide()
+      this.contentTargets.forEach((element) => {
+        element.style.display = ""
       })
-      $(this.contentTargets[idx]).show()
+      this.contentTargets[idx].style.display = "block"
     }
   })
 
   application.register("filter", class extends Stimulus.Controller {
     filter() {
-      let selectedPub = $(".pubfilter").val()
-      if(selectedPub) { $("#search_box").val("") }
+      let selectedPub = document.querySelector(".pubfilter").value
+      if(selectedPub) { document.querySelector("#search_box").value = "" }
       this._filterPublishers(selectedPub)
-      $("#title_sort").hide()
-      $("#pub_sort").show()
+      document.querySelector("#title_sort").style.display = ""
+      document.querySelector("#pub_sort").style.display = "block"
     }
 
     _filterPublishers(publisher) {
-      $(".about").hide()
-      $("#fade").hide()
-      $("#pub_sort").show()
-      $("#title_sort").hide()
+      document.querySelector(".about").style.display = ""
+      document.querySelector("#fade").style.display = ""
+      document.querySelector("#pub_sort").style.display = "block"
+      document.querySelector("#title_sort").style.display = ""
+
       if(publisher === "Show All") {
-        $("#books").children().show()
-        $("#sort").show()
-        $("#nav_info").hide()
+        Array.from(document.querySelector("#books").children).forEach((element, idx) => {
+          element.style.display = "block"
+        })
+        document.querySelector("#sort").style.display = "block"
+        document.querySelector("#nav_info").style.display = "none"
       } else {
-        $("#books").children().hide()
-        var matchingBooks = $("article p[itemprop='publisher']:contains(" + publisher +")").closest("article")
+        Array.from(document.querySelector("#books").children).forEach((book) => {
+          book.style.display = "none"
+        })
+
+        var allBooks = Array.from(document.querySelectorAll("article p[itemprop='publisher']"))
+        var matchingBooks = []
+        allBooks.forEach((candidate) => {
+          if(candidate.textContent == publisher)
+            matchingBooks.push(candidate)
+        })
         var numberFound = matchingBooks.length
 
-        $("#sort").hide()
-        matchingBooks.show()
+        document.querySelector("#sort").style.display = "none"
+        matchingBooks.forEach((element) => {
+          element.closest("article").style.display = "block"
+        })
         if(numberFound === 1) {
           var bookStr = "book"
         } else {
           var bookStr = "books"
         }
-        $("#nav_info").html("Found <strong>" + matchingBooks.length + "</strong> " + bookStr + " for <strong>" + publisher + "</strong>")
-        $("#nav_info").show()
+        document.querySelector("#nav_info").innerHTML = "Found <strong>" + matchingBooks.length + "</strong> " + bookStr + " for <strong>" + publisher + "</strong>"
+        document.querySelector("#nav_info").style.display = "block"
       }
     }
   })
 
   application.register("notbook", class extends Stimulus.Controller {
     close_books() {
-      $('.about').hide();
-      $('#fade').hide();
+      document.querySelectorAll('.about').forEach((element, idx) => {
+        element.style.display = "none"
+      })
+      document.querySelector('#fade').style.display = "none"
     }
   })
 
   application.register("search", class extends Stimulus.Controller {
-    static get targets() {
-      return [ "term" ]
-    }
+    static targets = ["term"]
 
     search_books() {
       this._clear()
 
       let matchingAuthors = []
       let matchingTitles = []
-      let matchingBooks = $("article ul[class='zero-results']")
+      let matchingBooks = []
 
       let rawTerm = this.termTarget.value
-      let searchOperator = this._getSearchOperator(rawTerm);
+      let searchOperator = this._getSearchOperator(rawTerm)
       let searchTerm = rawTerm.replace(searchOperator + ':', '').trim()
 
-      if(!searchOperator || searchOperator == 'author')
+      if(!searchOperator || searchOperator == 'author') {
         matchingAuthors = this._searchAuthors(searchTerm)
+      }
 
-      if(!searchOperator || searchOperator == 'title')
+      if(!searchOperator || searchOperator == 'title') {
         matchingTitles = this._searchTitles(searchTerm)
+      }
 
-      if (matchingAuthors.length > 0)
-        matchingBooks = matchingBooks.add(matchingAuthors)
+      if (matchingAuthors)
+        matchingAuthors = Array.from(new Set(matchingAuthors))
+        matchingBooks = matchingBooks.concat(matchingAuthors)
 
-      if (matchingTitles.length > 0)
-        matchingBooks = matchingBooks.add(matchingTitles)
+      if (matchingTitles)
+        matchingBooks = matchingBooks.concat(matchingTitles)
 
-      matchingBooks.sortElements(titleSort).show()
+      if (matchingBooks.length > 0) sortElements(matchingBooks, titleSort)
+
+      matchingBooks.forEach((element) => {
+        element.style.display = "block"
+      })
 
       let numberFound = matchingBooks.length
       if(numberFound === 1) {
@@ -163,58 +181,75 @@
         var bookStr = "books"
       }
 
-      $('#nav_info').html('Found <strong>' + numberFound + '</strong> ' + bookStr + ' matching "<strong>' + searchTerm + '</strong>"')
-      $('#nav_info').show()
+      document.querySelector('#nav_info').innerHTML = 'Found <strong>' + numberFound + '</strong> ' + bookStr + ' matching "<strong>' + searchTerm + '</strong>"'
+      document.querySelector('#nav_info').style.display = 'block'
     }
 
     _searchAuthors(term) {
-      return $("article ul[class='authors']:contains(" + term + ")").closest("article")
+      let allAuthors = []
+      let matchingAuthors = []
+
+      allAuthors = Array.from(document.querySelectorAll("article ul[class='authors']"))
+
+      allAuthors.forEach((author) => {
+        if(author.textContent.toLowerCase().includes(term.toLowerCase()))
+          matchingAuthors.push(author.closest("article"))
+      })
+
+      return matchingAuthors
     }
 
     _searchTitles(term) {
-      return $("article a[itemprop='name']:contains(" + term + ")").closest("article")
+      let allTitles = []
+      let matchingTitles = []
+
+      allTitles = Array.from(document.querySelectorAll("a[itemprop='name']"))
+
+      allTitles.forEach((title) => {
+        if(title.textContent.toLowerCase().includes(term.toLowerCase()))
+          matchingTitles.push(title.closest("article"))
+      })
+
+      return matchingTitles
     }
 
     _getSearchOperator(query) {
       let validOperators = ['title', 'author']
       let candidate = query.split(':')[0]
-      if(jQuery.inArray(candidate, validOperators) !== -1) {
+      if(validOperators.includes(candidate))
         return candidate
-      } else {
-        return null
-      }
     }
 
     _clear() {
-      $('.about').hide()
-      $('#fade').hide()
-      $('.pubfilter').val("")
-      $('#title_sort').hide()
-      $('#pub_sort').hide()
-      $('#nav_info').hide()
-      $('#books').children().hide()
+      document.querySelector('.about').style.display = "none"
+      document.querySelector('#fade').style.display = "none"
+      document.querySelector('.pubfilter').selectedIndex = -1
+      document.querySelector('#title_sort').style.display = "none"
+      document.querySelector('#pub_sort').style.display = "none"
+      document.querySelector('#nav_info').style.display = "none"
+      Array.from(document.querySelector('#books').children).forEach((element, idx) => {
+        element.style.display = "none"
+      })
     }
   })
 
   application.register("sort", class extends Stimulus.Controller {
-    static get targets() {
-      return [ "article" ]
-    }
+    static targets = [ "article" ]
 
     publisher_sort() {
-      $('.about').hide()
-      $('#fade').hide()
-      $(this.articleTargets).sortElements(publisherSort)
-      $('#pub_sort').hide()
-      $('#title_sort').show()
+      document.querySelector('.about').style.display = "none"
+      document.querySelector('#fade').style.display = "none"
+      sortElements(this.articleTargets, publisherSort)
+      document.querySelector('#pub_sort').style.display = "none"
+      document.querySelector('#title_sort').style.display = "block"
     }
 
     title_sort() {
-      $('.about').hide()
-      $('#fade').hide()
-      $(this.articleTargets).sortElements(titleSort)
-      $('#title_sort').hide()
-      $('#pub_sort').show()
+      document.querySelector('.about').style.display = "none"
+      document.querySelector('#fade').style.display = "none"
+      sortElements(this.articleTargets, titleSort)
+      document.querySelector('#title_sort').style.display = "none"
+      document.querySelector('#pub_sort').style.display = "block"
     }
   })
 })()
